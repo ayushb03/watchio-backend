30afd8e214ecb6319322062d3471869dba560e83
Ayush Bodade <144538401+ayushb03@users.noreply.github.com>
2024-03-31 18:54:02 +0530

    added registerUser controller and bug fixes

--- src/middlewares/multer.middlewares.js
+++ src/middlewares/multer.middlewares.js
@@ -1,4 +1,4 @@
-import multer from 'multer'
+import multer from "multer"
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -11,3 +11,5 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({ storage: storage })
+
+export { upload }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,19 @@
 import { Router } from "express"
 import { registerUser } from "../controllers/user.controllers.js"
+import { upload } from "../middlewares/multer.middlewares.js"
 
 const router = Router()
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields([
+    {
+        name: "avatar",
+        maxCnt: 1
+    }, 
+    {
+        name: "coverImage",
+        maxCnt: 1
+    }]),
+    registerUser)
 
 export default router   
