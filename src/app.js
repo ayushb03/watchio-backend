aa08e103d0155e4bb9df6ae1bc878916b2eea65c
Ayush Bodade <144538401+ayushb03@users.noreply.github.com>
2024-03-31 18:00:26 +0530

    backend setup configured

--- src/app.js
+++ src/app.js
@@ -18,4 +18,12 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// import routes
+
+import userRouter from "../src/routes/user.routes.js"
+
+// routes declaration
+
+app.use("/api/v1/users", userRouter)
+
 export default app
